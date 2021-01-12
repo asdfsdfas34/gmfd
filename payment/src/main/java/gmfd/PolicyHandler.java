5c93a6a000a383fa301e373a0a6657bc4f001d68
asdfsdfas34 <41769626+asdfsdfas34@users.noreply.github.com>
2021-01-12 13:46:05 +0900

    Add files via upload

---
+++ payment/src/main/java/gmfd/PolicyHandler.java
@@ -0,0 +1,45 @@
+package gmfd;
+
+import gmfd.config.kafka.KafkaProcessor;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PolicyHandler{
+    @StreamListener(KafkaProcessor.INPUT)
+    public void onStringEventListener(@Payload String eventString){
+
+    }
+
+
+    //Bean 간 연결
+    @Autowired
+    PayRepository payRepository;
+    //
+    @StreamListener(KafkaProcessor.INPUT)
+    public void wheneverDeliveryCanceled_Cancelled(@Payload DeliveryCanceled deliveryCanceled){
+
+        if(deliveryCanceled.isMe()){
+            System.out.println("##### listener Cancelled : " + deliveryCanceled.toJson());
+
+            Optional<Pay> optionalPay = payRepository.findById(deliveryCanceled.getOrderId());
+
+            Pay pay = optionalPay.get();
+
+            //pay.setOrderid(deliveryCanceled.getOrderId());
+            pay.setStatus("Delivery Canceled");
+
+            payRepository.save(pay);
+            //
+
+
+        }
+    }
+
+}
